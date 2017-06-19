447b3e99d3a863c6a97beecee9c383d619fb81ec
rupin147 <rupinmp@gmail.com>
2017-06-19 11:06:07 +0530

    Create fortinetclient.sh

---
+++ fortinetclient.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/expect 
+spawn /home/rupin/forticlientsslvpn/64bit/forticlientsslvpn_cli --server <IP Address>:<Port Number> --vpnuser rupin.puthukudi
+expect "Password for VPN:"
+send "<your password>\r"
+expect "Would you like to connect to this server? (Y/N)"
+send "y\r"
+interact
